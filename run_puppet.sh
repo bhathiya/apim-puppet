f5f9dc918127c124dcb5e383ec08da121a4cb019
Bhathiya Jayasekara <bhathiya@wso2.com>
2016-03-13 10:56:41 +0530

    Update run_puppet.sh

--- run_puppet.sh
+++ run_puppet.sh
@@ -1,5 +1,5 @@
 cd /opt/
-mysql -h 192.168.57.92 -u root -proot < script.sql
+mysql -h 192.168.57.92 -u root -proot < db_scripts/script.sql
 ssh -t -i datatgresearch.pem ubuntu@192.168.57.93 'cd /opt/; sudo bash setup.sh; exit;'
 ssh -t -i datatgresearch.pem ubuntu@192.168.57.94 'cd /opt/; sudo bash setup.sh; exit;'
 ssh -t -i datatgresearch.pem ubuntu@192.168.57.96 'cd /opt/; sudo bash setup.sh; exit;'
